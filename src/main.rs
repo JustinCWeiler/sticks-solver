ea35f235252009688c979921faedcff497e75085
Justin Weiler <jweiler@stanford.edu>
2023-03-29 21:59:56 -0700

    add state constructed/deconstructed form

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,12 @@ fn main() {
     s = s.action(Action::SPLIT(Hand::RIGHT, 1));
 
     println!("{:?}", s);
+
+    for val in 0..Player::<5>::MAX_PLAYER_VAL {
+        println!("{:?}", Player::<5> { val });
+    }
+
+    for val in 0..State::<3, 5>::MAX_STATE_VAL {
+        println!("{:?}", State::<3, 5> { val });
+    }
 }
--- src/state.rs
+++ src/state.rs
@@ -10,23 +10,23 @@ pub enum Action {
 
 // represents current game state
 #[derive(Debug, Clone)]
-pub struct State<const COUNT: usize, const MAX: usize> {
+struct StateD<const COUNT: usize, const MAX: usize> {
     players: [Player<MAX>; COUNT],
     turn: usize,
 }
 
 // player 0 starts
-impl<const COUNT: usize, const MAX: usize> Default for State<COUNT, MAX> {
+impl<const COUNT: usize, const MAX: usize> Default for StateD<COUNT, MAX> {
     fn default() -> Self {
-        State {
+        StateD {
             players: [Player::default(); COUNT],
             turn: 0,
         }
     }
 }
 
-impl<const COUNT: usize, const MAX: usize> State<COUNT, MAX> {
-    pub fn action(mut self, action: Action) -> State<COUNT, MAX> {
+impl<const COUNT: usize, const MAX: usize> StateD<COUNT, MAX> {
+    pub fn action(mut self, action: Action) -> StateD<COUNT, MAX> {
         match action {
             Action::ATTACK(attacking_hand, other_player_idx, other_hand) => {
                 if other_player_idx != self.turn && other_player_idx < COUNT {
@@ -54,5 +54,75 @@ impl<const COUNT: usize, const MAX: usize> State<COUNT, MAX> {
         }
 
         self
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct State<const COUNT: usize, const MAX: usize> {
+    pub val: usize,
+}
+
+impl<const COUNT: usize, const MAX: usize> std::fmt::Debug for State<COUNT, MAX> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        let s = self.deconstruct();
+        s.fmt(f)
+    }
+}
+
+impl<const COUNT: usize, const MAX: usize> Default for State<COUNT, MAX> {
+    fn default() -> Self {
+        let mut val = 0;
+
+        for _ in 0..COUNT {
+            val *= Self::PLAYER_MAX_VAL;
+            val += MAX + 1;
+        }
+
+        State { val }
+    }
+}
+
+impl<const COUNT: usize, const MAX: usize> State<COUNT, MAX> {
+    const PLAYER_MAX_VAL: usize = MAX * (MAX + 1) / 2;
+    const TOTAL_PLAYER_MAX_VAL: usize = Self::PLAYER_MAX_VAL.pow(COUNT as u32);
+    pub const MAX_STATE_VAL: usize = Self::TOTAL_PLAYER_MAX_VAL * COUNT;
+
+    fn construct(state: StateD<COUNT, MAX>) -> State<COUNT, MAX> {
+        let mut val = 0;
+
+        for i in 0..COUNT {
+            val *= Self::PLAYER_MAX_VAL;
+            val += state.players[i].val;
+        }
+
+        val += Self::TOTAL_PLAYER_MAX_VAL * state.turn;
+
+        State { val }
+    }
+
+    fn deconstruct(self) -> StateD<COUNT, MAX> {
+        let mut val = self.val;
+
+        let mut new_state = StateD::default();
+
+        new_state.turn = val / Self::TOTAL_PLAYER_MAX_VAL;
+        val %= Self::TOTAL_PLAYER_MAX_VAL;
+
+        let mut radix = Self::TOTAL_PLAYER_MAX_VAL;
+        for i in 0..COUNT {
+            radix /= Self::PLAYER_MAX_VAL;
+            new_state.players[i].val = val / radix;
+            val %= radix;
+        }
+
+        new_state
+    }
+
+    pub fn action(self, action: Action) -> State<COUNT, MAX> {
+        let state_deconst = self.deconstruct();
+
+        let new_state_deconst = state_deconst.action(action);
+
+        Self::construct(new_state_deconst)
     }
 }
